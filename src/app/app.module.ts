3422ff9d468a6dc2ed315315bf98a7f12b2af237
Juri <juri.strumpflohner@gmail.com>
2018-10-06 14:52:04 +0200

    feat: configure http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, SharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
