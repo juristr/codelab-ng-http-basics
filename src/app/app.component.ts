3a5b7ca470a2639cef72eb61a61a5377e61f6eca
Juri <juri.strumpflohner@gmail.com>
2018-10-06 15:11:41 +0200

    add types to http call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeopleService } from './people.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular Http Basics';
+
+  constructor(private peopleService: PeopleService) {}
+
+  ngOnInit(): void {
+    this.peopleService.getPeople().subscribe(x => console.log(x));
+  }
 }
--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface Person {
+  id?: number;
+  name: string;
+  website?: string;
+  twitter?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,6 +15,6 @@ export class PeopleService {
   constructor(private http: HttpClient) {}
 
   getPeople() {
-    return this.http.get('/api/people');
+    return this.http.get<Person[]>('/api/people');
   }
 }
