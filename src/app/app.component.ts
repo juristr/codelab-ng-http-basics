dd0e2adb675938834f7f9529e5826483044469e5
Juri <juri.strumpflohner@gmail.com>
2018-10-06 15:38:30 +0200

    create UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,8 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.people = this.peopleService.getPeople();
   }
+
+  createPerson(name: string) {
+    console.log('new person', name);
+  }
 }
